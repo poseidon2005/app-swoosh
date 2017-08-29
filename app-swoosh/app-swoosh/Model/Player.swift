acc34432abafc396032935f10b1e4ef65486c153
Jay Hall <jay@alpacalypse.com>
2017-08-29 12:51:26 +1000

    build struct and enable next after selection function applied

---
+++ app-swoosh/app-swoosh/Model/Player.swift
@@ -0,0 +1,15 @@
+//
+//  File.swift
+//  app-swoosh
+//
+//  Created by Jay Hall on 29/8/17.
+//  Copyright © 2017 Jay Hall. All rights reserved.
+//
+
+import Foundation
+
+struct Player {
+    var desiredLeague: String?
+    var selectedSkillLevel: String?
+    
+}
